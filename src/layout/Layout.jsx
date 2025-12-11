a74ef335480633dda2a0304d3b22ffe0da5c4414
angelostori <angelostori13@gmail.com>
2025-12-11 12:48:02 +0100

    create a base layout with a sample navigation to the page Home that already don't exist

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <Link to="/">Home</Link>
+                </nav>
+            </header>
+
+            <main>
+                <Outlet />
+                {/*Visualizzazione pagine*/}
+            </main>
+
+            <footer>
+                <ul>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contacts</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy</a></li>
+                </ul>
+                <p>© 2025 My Movies App</p>
+            </footer>
+        </div>
+    )
+}
